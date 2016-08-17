a1160550d197482e543ac2f148fa9042f3860f19
Anthony Stewart <astewa44@vols.utk.edu>
2016-08-16 21:59:38 -0400

    Removed mega folder

--- mega/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-ino build -m mega2560 --cppflags="-D __USER__=`whoami` -D __DIR_`pwd` -D __GIT_HASH__=`git rev-parse HEAD`"
--- mega/upload.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-sh build.sh
-git add -A
-git commit -m "new uploaded arduino code for mega"
-git push
-ino upload -m mega2560 -p /dev/mega
