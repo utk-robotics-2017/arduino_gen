1b6b7d53c5b94e863f0604eaa3a0c0bb5dc7411f
Andrew <amessing@utk.edu>
2016-09-20 13:13:48 -0400

    added .travis.yml

---
+++ travis_ci.py
@@ -0,0 +1,10 @@
+import os
+from ArduinoGen import ArduinoGen
+confFolder = "./conf"
+confFolderAbsPath = os.path.abspath(confFolder)
+deviceJsonFile = confFolderAbsPath + "/template.json"
+ag = ArduinoGen(arduino="template")
+ag.setParentFolder(os.path.dirname(os.path.realpath(__file__)))
+ag.setupFolder()
+ag.readConfig(deviceJsonFile)
+ag.generateOutput()
