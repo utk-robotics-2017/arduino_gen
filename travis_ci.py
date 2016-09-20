b500772417096d543dc0e8db5c51ddefae517413
Andrew <amessing@utk.edu>
2016-09-20 14:18:00 -0400

    setup travis_ci stuff to actually go to the repo

--- travis_ci.py
+++ travis_ci.py
@@ -1,8 +1,6 @@
 import os
 from ArduinoGen import ArduinoGen
-confFolder = "./conf"
-confFolderAbsPath = os.path.abspath(confFolder)
-deviceJsonFile = confFolderAbsPath + "/travis_ci.json"
+deviceJsonFile = "travis_ci.json"
 ag = ArduinoGen(arduino="travis_ci")
 ag.setParentFolder(os.path.dirname(os.path.realpath(__file__)))
 ag.setupFolder()
