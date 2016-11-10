bcc7d0aaba966bf371eba8860901370ebc97d3ad
Aiden Rutter <arutter@utk.edu>
2016-11-09 20:35:23 -0500

    before eats commit
    gotta get those eats

--- appendages/lcd_display_list.py
+++ appendages/lcd_display_list.py
@@ -12,18 +12,34 @@ class LcdDisplayList(ComponentList):
     TIER = 1
 
     def __init__(self):
-        self.digital_sensor_list = []
-        self.analog_sensor_list = []
+        self.digital_lcd_list = []
+        self.analog_lcd_list = []
 
     def add(self, json_item):
         if(json_item['digital']):
             self.digital_sensor_list.append(LineSensor(json_item['label'], json_item['pin']))
         else:
             self.analog_sensor_list.append(LineSensor(json_item['label'], json_item['pin']))
-    def get_pins():
-    def get_includes
+    def get_pins(self):
+        rv = ""
+        for lcd16 in self.digital_lcd_list:
+            rv += "const char {0:s}_pin = {1:d};\n".format(sensor.label, sensor.pin)
+        for lcd16 in self.analog_lcd_list:
+            rv += "const char {0:s}_pin = {1:d};\n".format(sensor.label, sensor.pin)
+        rv += "\n"
+
+    def get_includes(self):
+        return '#include "lcd16.h"\n'
+
     def get_commands(self):
         rv = ""
         if(len(self.digital_sensor_list) > 0)
             rv +=
         if(len(self.digital_sensor_list) > 0)
+            rv +=
+        return rv
+    def get_constructor(self):
+    def get_setup(self):
+    def get_command_attaches():
+    def get_command_functions():
+    def get_core_values():
