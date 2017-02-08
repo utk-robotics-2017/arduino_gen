fafc95dea3b9b33e23a8b4e53171db53178ce0a2
Anthony Stewart <astewa44@vols.utk.edu>
2017-02-08 11:39:45 +0000

    Added intpacking and documentation to ECD.

--- appendages/electronic_component_detector_list.py
+++ appendages/electronic_component_detector_list.py
@@ -1,5 +1,31 @@
 from appendages.component_list import ComponentList
 
+'''
+The value from Decode will be packed into an int in the following format.
+
+Each pad's code fits in 3 bits, with the last bit unused.
+0|555|444|333|222|111
+
+Component | Code
+----------|-----
+  Unknown | 0
+     Wire | 1
+ Resistor | 2
+Capacitor | 3
+ Inductor | 4
+    Diode | 5
+
+For example, let's say the components are on the following pads:
+1: Inductor
+2: Resistor
+3: Capacitor
+4: Wire
+5: Diode
+
+The code would be 42315, packed into an int would be:
+ |  5|  1|  3|  2|  4
+0|101|001|011|010|100; Without seperators: 0101001011010100
+'''
 
 class ElectronicComponentDetector:
     def __init__(self, label):
@@ -40,13 +66,13 @@ class ElectronicComponentDetectorList(ComponentList):
         rv = "void Decode() {\n"
         rv += "\tchar pad = cmdMessenger.readBinArg<char>();\n"
         rv += "\tchar code[5];\n"
+        rv += "\tint rv = 0;\n"
         rv += "\tecd.decode(pad, code, false);\n"
         rv += "\tcmdMessenger.sendBinCmd(kAcknowledge, kDecode);\n"
-        rv += "\tcmdMessenger.sendCmdStart(kDecodeResult);\n"
-        rv += "\tfor(int x = 0 ; x <= 4 ; x++ ) { \n"
-        rv += "\t\tcmdMessenger.sendCmdBinArg(code[x]);\n"
+        rv += "\tfor(int i = 0; i < 5; i++) { \n"
+        rv += "\t\trv |= code[i] << (i * 3);\n"
         rv += "\t}\n"
-        rv += "\tcmdMessenger.sendCmdEnd();\n"
+        rv += "\tcmdMessenger.sendBinCmd(kDecodeResult, rv);"
         rv += "}\n\n"
         return rv
 
