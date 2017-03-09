3d86147ee8487c55cabfff4225db4ee92c544b32
Ben Klein <robobenklein@gmail.com>
2017-03-08 20:19:12 -0500

    No shell eval pls

--- travis_ci.sh
+++ travis_ci.sh
@@ -2,7 +2,7 @@
 coverage3 run travis_ci.py
 coverage3 report
 echo "=== Generated Code: ==="
-sed -i $'s/\t/    /g' travis_ci/src/travis_ci.ino
+sed -i 's/\t/    /g' travis_ci/src/travis_ci.ino
 cat travis_ci/src/travis_ci.ino
 cp platformio.ini travis_ci
 echo "=== Compiling: ==="
