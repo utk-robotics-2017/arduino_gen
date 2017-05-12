d8b2c5571efe7f00e5d243f78c077c9680b2287c
Andrew Messing <amessing@utk.edu>
2017-05-12 12:59:47 -0400

    removed junk

--- stage2/upload.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd /Robot/CurrentArduinoCode/stage2
-git add -A
-git commit -m "new uploaded arduino code for stage2"
-git push
-pio run -t upload
--- stage4/upload.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd /Robot/CurrentArduinoCode/stage4
-git add -A
-git commit -m "new uploaded arduino code for stage4"
-git push
-pio run -t upload
